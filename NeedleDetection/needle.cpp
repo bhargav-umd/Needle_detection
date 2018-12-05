3cbc2743b74490387b36eb2546ca1b2fb499e2f0
bhargav-umd <bhargav@umd.edu>
2018-12-05 04:40:31 -0500

    cropped a polygon on petri dish edges and detected needle of length 30 to 40

--- NeedleDetection/needle.cpp
+++ NeedleDetection/needle.cpp
@@ -9,29 +9,21 @@ int main() {
         cv::imread("4_PI1_screenshot_29.11.2018.png", CV_LOAD_IMAGE_COLOR);
     cv::Mat needle_detect_ = img.clone();
 
-    cv::Vec3f circ(354, 275, 100);
-
-    cv::Mat1b mask(img.size(), uchar(0));
-    circle(mask, cv::Point(circ[0], circ[1]), circ[2], cvScalar(255),
-           CV_FILLED);
-
-    // Compute the bounding box
-    cv::Rect bbox(circ[0] - circ[2], circ[1] - circ[2], 2 * circ[2],
-                  2 * circ[2]);
-
-    // Create a black image
-    cv::Mat3b res(img.size(), cv::Vec3b(0, 0, 0));
-
-    // Copy only the image under the white circle to black image
-    img.copyTo(res, mask);
-
-    // Crop according to the roi
-    res = res(bbox);
-
-    cv::imshow("cropped circle", res);
-
+    cv::Mat roi;   // Matrix to save image
+    // creating empty mask image
+    cv::Mat mask = cv::Mat::zeros(img.size(), img.type());
+    // determining polygon vertices for roi
+    cv::Point pts[7] = {cv::Point(288, 200), cv::Point(355, 176),
+                        cv::Point(423, 205), cv::Point(442, 325),
+                        cv::Point(352, 375), cv::Point(275, 337),
+                        cv::Point(252, 337)};
+    // Create a binary polygon mask
+    cv::fillConvexPoly(mask, pts, 7, cv::Scalar(255, 255, 255));
+    // Multiply the edges image and the mask to get the output
+    cv::bitwise_and(img, mask, roi);
+    cv::imshow("roi", roi);
     cv::Mat edge, needle, edge_image;
-    cv::Canny(res, edge, 50, 140, 3);
+    cv::Canny(roi, edge, 50, 140, 3);
     edge.convertTo(edge_image, CV_8U);
     // Draw an example circle on the video stream
     cv::circle(needle_detect_, cv::Point(354, 275), 100, CV_RGB(255, 0, 0));
@@ -44,18 +36,20 @@ int main() {
     std::cout << "the number of lines are" << lines.size() << std::endl;
     for (size_t i = 0; i < lines.size(); i++) {
         cv::Vec4i l = lines[i];
-
         double leng = cv::norm(cv::Point(l[0], l[1]) - cv::Point(l[2], l[3]));
-        std::cout << " length of line number: " << i << " is : " << leng
-                  << std::endl;
-        line(needle_detect_, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]),
-             cvScalar(255, 255, 255), 2, CV_AA);
-        std::cout << "the coordinates of the lines are: "
-                  << cv::Point(l[0], l[1]) << " and " << cv::Point(l[2], l[3])
-                  << std::endl;
+
         if (leng < 40 && leng > 30) {
             line(needle_detect_, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]),
                  cvScalar(0, 0, 255), 1, CV_AA);
+            std::cout << " length of line number: " << i << " is : " << leng
+                      << std::endl;
+            // line(needle_detect_, cv::Point(l[0], l[1]), cv::Point(l[2],
+            // l[3]),
+            //   cvScalar(255, 255, 255), 2, CV_AA);
+
+            std::cout << "the coordinates of the lines are: "
+                      << cv::Point(l[0], l[1]) << " and "
+                      << cv::Point(l[2], l[3]) << std::endl;
         }
     }
     cv::imshow("edge", edge);
